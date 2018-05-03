6d3eb6e64d10dbcce3981513e25385a5ea805f05
Sterling Baldwin <baldwin32@llnl.gov>
2018-05-02 17:21:12 -0700

    Adds pretty prints

--- lib/util.py
+++ lib/util.py
@@ -20,3 +20,28 @@ def format_debug(e):
     exec_1=sys.exc_info()[1],
     lineno=traceback.tb_lineno(sys.exc_info()[2]),
     stack=traceback.print_tb(tb))
+
+class colors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+
+def print_message(message, status='error'):
+    """
+    Prints a message with either a green + or a red -
+
+    Parameters:
+        message (str): the message to print
+        status (str): th"""
+    if status == 'error':
+        print colors.FAIL + '[-] ' + colors.ENDC + colors.BOLD + str(message) + colors.ENDC
+    elif status == 'ok':
+        print colors.OKGREEN + '[+] ' + colors.ENDC + str(message)
+    elif status == 'debug':
+        print colors.OKBLUE + '[*] ' + colors.ENDC + str(message) + colors.OKBLUE + ' [*]' + colors.ENDC
