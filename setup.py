0f5ec673cf647bf4958d7cbb660a3d20aac58315
Xylar Asay-Davis <xylarstorm@gmail.com>
2019-03-07 19:18:58 -0700

    Update setup to add entry point
    A few other pieces of cleanup

--- setup.py
+++ setup.py
@@ -2,17 +2,20 @@ import sys
 import os
 from setuptools import find_packages, setup
 
+
 cmor_handlers = [(sys.prefix + '/share/e3sm_to_cmip/cmor_handlers',
-                  [os.path.join('cmor_handlers', x) for x in os.listdir('cmor_handlers')])]
+                  [os.path.join('cmor_handlers', x) for x in
+                   os.listdir('cmor_handlers')])]
 
 
 setup(
     name="e3sm_to_cmor",
-    version="0.0.5",
+    version="1.0.1",
     author="Sterling Baldwin",
     author_email="baldwin32@llnl.gov",
-    description="Transform E3SM model data output into cmip6 compatable data using the Climate Model Output Rewritter.",
-    scripts=["e3sm_to_cmip"],
-    packages=find_packages(
-        exclude=["*.test.*"]),
+    description="Transform E3SM model data output into cmip6 compatable data "
+                "using the Climate Model Output Rewriter.",
+    entry_points={'console_scripts':
+                  ['e3sm_to_cmip = e3sm_to_cmip.__main__:main']},
+    packages=find_packages(),
     data_files=cmor_handlers)
