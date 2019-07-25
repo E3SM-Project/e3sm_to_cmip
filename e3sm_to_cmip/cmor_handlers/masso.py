Fix masso and volo masking
It isn't clear to me why this change fixes the rounding issue but
it seems to.

e3sm_to_cmip/cmor_handlers/masso.py
@@ -58,7 +58,8 @@ def handle(infiles, tables, user_input_path, **kwargs):
     ds = xarray.Dataset()
     with mpas.open_mfdataset(timeSeriesFiles, variableList) as dsIn:
         ds[VAR_NAME] = (config_density0 *
-                        dsIn.timeMonthly_avg_layerThickness.where(cellMask3D) *
+                        dsIn.timeMonthly_avg_layerThickness.where(
+                            cellMask3D, 0.) *
                         dsMesh.areaCell).sum(dim=['nVertLevels', 'nCells'])
         ds = mpas.add_time(ds, dsIn)
         ds.compute()