c9d3536432695694484128f13a42313f871c6bbc
Sterling Baldwin <baldwin32@llnl.gov>
2019-08-12 12:53:01 -0700

    Adds the default handler yaml file to the package_data

--- setup.py
+++ setup.py
@@ -4,11 +4,14 @@ from setuptools import find_packages, setup
 
 setup(
     name="e3sm_to_cmip",
-    version="1.2.0",
+    version="1.2.1",
     author="Sterling Baldwin",
     author_email="baldwin32@llnl.gov",
     description="Transform E3SM model data output into cmip6 compatable data "
                 "using the Climate Model Output Rewriter.",
     entry_points={'console_scripts':
                   ['e3sm_to_cmip = e3sm_to_cmip.__main__:main']},
-    packages=find_packages())
+    packages=['e3sm_to_cmip'],
+    package_dir={'e3sm_to_cmip': 'e3sm_to_cmip'},
+    package_data={'e3sm_to_cmip': ['LICENSE', 'cmor_handlers/default_handler_info.yaml']},
+    include_package_data=True)
