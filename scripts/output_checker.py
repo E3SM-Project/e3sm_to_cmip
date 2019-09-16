e6b30d8ba9f0cd4e086aa33fddac37ebce53faa3
Sterling Baldwin <baldwin32@llnl.gov>
2019-09-16 14:25:38 -0700

    Adds warning if multiple directories found

--- scripts/output_checker.py
+++ scripts/output_checker.py
@@ -145,15 +145,14 @@ def check_case(path, variables, spec, case, ens, published):
             if 'r{}i1p1f1'.format(ens) not in root.split(os.sep):
                 continue
 
-            root_base = os.path.split(root)[0]
-            num_folders = 0
-            for _,dirnames,filenames in os.walk(root_base):
-                num_folders += len(dirnames)
-                files += filenames
-            #print(num_folders)
   
             files = sorted(files)
             var = files[0].split('_')[0]
+
+            root_base = os.path.split(root)[0]
+            if len(root_base) > 1:
+                print("WARNING: multiple directories found for {case}-{ens}-{var}".format(
+                    case=case, ens=ens, var=var))
 
             table = root.split(os.sep)[-4]
 
