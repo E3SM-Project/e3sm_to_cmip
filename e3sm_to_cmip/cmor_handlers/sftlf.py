197fe60e8bd5ef20b00ce6f58c27043bc035b0a4
Sterling Baldwin <baldwin32@llnl.gov>
2019-09-18 14:46:16 -0700

    Adds fx table variables

---
+++ e3sm_to_cmip/cmor_handlers/sftlf.py
@@ -0,0 +1,157 @@
+"""
+LANDFRAC to sftlf converter
+"""
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+import cmor
+import os
+import logging
+import cdms2
+import progressbar
+from e3sm_to_cmip.util import print_message
+from e3sm_to_cmip.lib import handle_variables
+
+# list of raw variable names needed
+RAW_VARIABLES = [str('sftlf')]
+VAR_NAME = str('LANDFRAC')
+VAR_UNITS = str('%')
+TABLE = str('CMIP6_fx.json')
+
+
+def my_dynamic_message(self, progress, data):
+    """
+    Make the progressbar not crash, and also give a nice custom message
+    """
+    val = data['dynamic_messages'].get('running')
+    if val:
+        return 'Running: {0: <16}'.format(data['dynamic_messages'].get('running'))
+    else:
+        return 'Running: ' + 16 * '-'
+# ------------------------------------------------------------------
+
+
+def handle(infiles, tables, user_input_path, **kwargs):
+
+    logger = logging.getLogger()
+    msg = '{}: Starting'.format(VAR_NAME)
+    logger.info(msg)
+
+    logdir = kwargs.get('logdir')
+    serial = kwargs.get('serial')
+
+    # check that we have some input files for every variable
+    zerofiles = False
+    for variable in RAW_VARIABLES:
+        if len(infiles[variable]) == 0:
+            msg = '{}: Unable to find input files for {}'.format(
+                VAR_NAME, variable)
+            print_message(msg)
+            logging.error(msg)
+            zerofiles = True
+    if zerofiles:
+        return None
+
+    # Create the logging directory and setup cmor
+    if logdir:
+        logpath = logdir
+    else:
+        outpath, _ = os.path.split(logger.__dict__['handlers'][0].baseFilename)
+        logpath = os.path.join(outpath, 'cmor_logs')
+    os.makedirs(logpath, exist_ok=True)
+
+    logfile = os.path.join(logpath, VAR_NAME + '.log')
+
+    cmor.setup(
+        inpath=tables,
+        netcdf_file_action=cmor.CMOR_REPLACE,
+        logfile=logfile)
+
+    cmor.dataset_json(str(user_input_path))
+    cmor.load_table(str(TABLE))
+
+    msg = '{}: CMOR setup complete'.format(VAR_NAME)
+    logging.info(msg)
+
+    # extract data from the input file
+    msg = 'sftlf: loading LANDFRAC'
+    logger.info(msg)
+
+    filename = infiles['LANDFRAC'][0]
+
+    if not os.path.exists(filename):
+        raise IOError("File not found: {}".format(filename))
+
+    f = cdms2.open(filename)
+
+    # load the data for each variable
+    variable_data = f('LANDFRAC')
+
+    if not variable_data.any():
+        raise IOError("Variable data not found: {}".format(variable))
+
+    # load the lon and lat info & bounds
+    data = {
+        'lat': variable_data.getLatitude(),
+        'lon': variable_data.getLongitude(),
+        'lat_bnds': f('lat_bnds'),
+        'lon_bnds': f('lon_bnds'),
+        'LANDFRAC': f('LANDFRAC')
+    }
+
+    msg = '{name}: loading axes'.format(name=VAR_NAME)
+    logger.info(msg)
+
+    axes = [{
+        str('table_entry'): str('latitude'),
+        str('units'): data['lat'].units,
+        str('coord_vals'): data['lat'][:],
+        str('cell_bounds'): data['lat_bnds'][:]
+    }, {
+        str('table_entry'): str('longitude'),
+        str('units'): data['lon'].units,
+        str('coord_vals'): data['lon'][:],
+        str('cell_bounds'): data['lon_bnds'][:]
+    }]
+
+    msg = 'sftlf: running CMOR'
+    logging.info(msg)
+
+    axis_ids = list()
+    for axis in axes:
+        axis_id = cmor.axis(**axis)
+        axis_ids.append(axis_id)
+
+    varid = cmor.variable(VAR_NAME, VAR_UNITS, axis_ids)
+
+    if serial:
+        myMessage = progressbar.DynamicMessage('running')
+        myMessage.__call__ = my_dynamic_message
+        widgets = [
+            progressbar.DynamicMessage('running'), ' [',
+            progressbar.Timer(), '] ',
+            progressbar.Bar(),
+            ' (', progressbar.ETA(), ') '
+        ]
+        progressbar.DynamicMessage.__call__ = my_dynamic_message
+        pbar = progressbar.ProgressBar(
+            maxval=len(1), widgets=widgets)
+        pbar.start()
+
+    outdata = data['LANDFRAC']
+    cmor.write(
+        varid,
+        outdata)
+
+    if serial:
+        pbar.update(1, running=msg)
+        pbar.finish()
+
+    msg = '{}: write complete, closing'.format(VAR_NAME)
+    logger.debug(msg)
+
+    cmor.close()
+
+    msg = '{}: file close complete'.format(VAR_NAME)
+    logger.debug(msg)
+
+    return 'sftlf'
