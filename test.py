51203d919363942be9ea69c485f051a94f56f42c
Sterling Baldwin <baldwin32@llnl.gov>
2021-06-18 13:09:36 -0700

    Adds branch swapping

--- test.py
+++ test.py
@@ -17,6 +17,7 @@ to  be ingested by e3sm_to_cmip.
 At the moment it only tests atmospheric monthly variables, but more will be added in the future'''
 
 def run_cmd(cmd: str):
+    print(f"running: '{' '.join(cmd)}'")
     proc = Popen(cmd, stdout=PIPE, stderr=PIPE)
     output = []
     while proc.poll() is None:
@@ -32,7 +33,7 @@ def run_cmd(cmd: str):
     return proc.returncode, ''.join(output)
 
 
-def test(vars: List, branch: str, input: Path, output: Path):
+def test(vars: List, comp_branch: str, input: Path, output: Path):
     if not input.exists():
         raise ValueError(f"Input directory {input} does not exist")
     output.mkdir(exist_ok=True)
@@ -41,12 +42,35 @@ def test(vars: List, branch: str, input: Path, output: Path):
     retcode, output = run_cmd(cmd)
     if retcode:
         print('Error checking the git status')
+        return 1
     if output and '??' in output or 'M' in output:
         print('git status is not clean, commit or stash your changes and try again')
         return 1
     
+    # store what the source branch name is
+    cmd = 'git branch --show-current'
+    retcode, output = run_cmd(cmd)
+    if retcode:
+        print('Error getting the source git branch')
+        return 1
+    source_banch = output
 
+    # check out the branch to compare this one against
+    cmd = f'git checkout {comp_branch}'
+    retcode, output = run_cmd(cmd)
+    if retcode:
+        print(f'Error checking out {comp_branch}')
+        return 1
     
+    # do the testing
+
+    # swap back to the source branch
+    cmd = f'git checkout {source_banch}'
+    retcode, output = run_cmd(cmd)
+    if retcode:
+        print(f'Error checking out {source_banch}')
+        return 1
+
     return 0
 
 
@@ -80,7 +104,7 @@ def main():
     try:
         retval = test(
             vars=parsed_args.var_list, 
-            branch=parsed_args.compare, 
+            comp_branch=parsed_args.compare, 
             input=Path(parsed_args.input),
             output=Path(parsed_args.output))
     except Exception as e:
