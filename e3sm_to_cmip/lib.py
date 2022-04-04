6a9801a3c8eaefa1eef34c766f6887ca40bbe1ac
tomvothecoder <tomvothecoder@gmail.com>
2022-04-04 11:13:01 -0500

    Remove line that strips `_highfreq` substring

--- e3sm_to_cmip/lib.py
+++ e3sm_to_cmip/lib.py
@@ -340,9 +340,6 @@ def var_has_time(table_path, variable):
     with open(table_path, 'r') as inputstream:
         table_info = json.load(inputstream)
 
-    # NOTE: This removes the highfreq portion from the variable
-    if '_highfreq' in variable:
-        variable = variable[:len('_highfreq') * -1]
     axis_info = table_info['variable_entry'][variable]['dimensions'].split(' ')
     if 'time' in axis_info:
         return 'time'
