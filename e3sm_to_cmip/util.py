0a2e162270f7ddc7ef3b2f5cb7ff54d9f871f375
tomvothecoder <tomvothecoder@gmail.com>
2022-05-16 18:42:41 -0500

    Add `pragma: no cover` for `__version__`

--- e3sm_to_cmip/util.py
+++ e3sm_to_cmip/util.py
@@ -16,7 +16,8 @@ import xarray as xr
 import yaml
 from tqdm import tqdm
 
-from e3sm_to_cmip import __version__, resources
+from e3sm_to_cmip import __version__  # pragma: no cover
+from e3sm_to_cmip import resources
 from e3sm_to_cmip._logger import _setup_custom_logger
 
 logger = _setup_custom_logger(__name__)
