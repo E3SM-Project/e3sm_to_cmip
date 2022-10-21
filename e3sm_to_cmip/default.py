3da2f522820e78ba0cea62151fc15b11601a1f63
Chengzhu Zhang <zhang40@llnl.gov>
2022-10-21 12:22:16 -0700

    address review; fixing formatting

--- e3sm_to_cmip/default.py
+++ e3sm_to_cmip/default.py
@@ -1,75 +1,78 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
-from e3sm_to_cmip.lib import handle_variables
+
 import cmor
 import numpy as np
+
+from e3sm_to_cmip.lib import handle_variables
 
 
 def default_handler(infiles, tables, user_input_path, **kwargs):
 
-    RAW_VARIABLES = kwargs['raw_variables']
-    unit_conversion = kwargs.get('unit_conversion')
+    RAW_VARIABLES = kwargs["raw_variables"]
+    unit_conversion = kwargs.get("unit_conversion")
 
     def write_data(varid, data, timeval=None, timebnds=None, index=None, **kwargs):
 
         if timeval is not None:
             if unit_conversion is not None:
-                if unit_conversion == 'g-to-kg':
+                if unit_conversion == "g-to-kg":
                     outdata = data[RAW_VARIABLES[0]].values[index, :] / 1000.0
-                elif unit_conversion == '1-to-%':
+                elif unit_conversion == "1-to-%":
                     outdata = data[RAW_VARIABLES[0]].values[index, :] * 100.0
-                elif unit_conversion == 'm/s-to-kg/ms':
+                elif unit_conversion == "m/s-to-kg/ms":
                     outdata = data[RAW_VARIABLES[0]].values[index, :] * 1000
-                elif unit_conversion == '-1':
+                elif unit_conversion == "-1":
                     outdata = data[RAW_VARIABLES[0]].values[index, :] * -1
                 else:
                     raise ValueError(
-                        f"{unit_conversion} isn't a supported unit conversion for default variables")
+                        f"{unit_conversion} isn't a supported unit conversion for default variables"
+                    )
             else:
                 outdata = data[RAW_VARIABLES[0]].values[index, :]
         else:
             if unit_conversion is not None:
-                if unit_conversion == 'g-to-kg':
+                if unit_conversion == "g-to-kg":
                     outdata = data[RAW_VARIABLES[0]].values / 1000.0
-                elif unit_conversion == '1-to-%':
+                elif unit_conversion == "1-to-%":
                     outdata = data[RAW_VARIABLES[0]].values * 100.0
-                elif unit_conversion == 'm/s-to-kg/ms':
+                elif unit_conversion == "m/s-to-kg/ms":
                     outdata = data[RAW_VARIABLES[0]].values * 1000
-                elif unit_conversion == '-1':
+                elif unit_conversion == "-1":
                     outdata = data[RAW_VARIABLES[0]].values * -1
                 else:
                     raise ValueError(
-                        f"{unit_conversion} isn't a supported unit conversion for default variables")
+                        f"{unit_conversion} isn't a supported unit conversion for default variables"
+                    )
             else:
                 outdata = data[RAW_VARIABLES[0]].values
 
-        outdata[np.isnan(outdata)] = 1.e20 
+        outdata[np.isnan(outdata)] = 1.0e20
 
-        if kwargs.get('simple'):
+        if kwargs.get("simple"):
             return outdata
 
         if timeval:
-            cmor.write(
-                varid,
-                outdata,
-                time_vals=timeval,
-                time_bnds=timebnds)
+            cmor.write(varid, outdata, time_vals=timeval, time_bnds=timebnds)
         else:
             cmor.write(varid, outdata)
-        
+
         return outdata
 
     return handle_variables(
         metadata_path=user_input_path,
         tables=tables,
-        table=kwargs['table'],
+        table=kwargs["table"],
         infiles=infiles,
         raw_variables=RAW_VARIABLES,
         write_data=write_data,
-        outvar_name=kwargs['name'],
-        outvar_units=kwargs['units'],
-        serial=kwargs.get('serial'),
-        positive=kwargs.get('positive'),
-        logdir=kwargs.get('logdir'),
-        simple=kwargs.get('simple'),
-        outpath=kwargs.get('outpath'))
+        outvar_name=kwargs["name"],
+        outvar_units=kwargs["units"],
+        serial=kwargs.get("serial"),
+        positive=kwargs.get("positive"),
+        logdir=kwargs.get("logdir"),
+        simple=kwargs.get("simple"),
+        outpath=kwargs.get("outpath"),
+    )
+
+
 # ------------------------------------------------------------------
