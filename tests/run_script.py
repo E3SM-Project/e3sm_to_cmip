994943e1ec4e9af9ef18f3f5c4181a72c5c82b65
tomvothecoder <tomvothecoder@gmail.com>
2024-11-11 15:46:41 -0600

    Update run script

--- tests/run_script.py
+++ tests/run_script.py
@@ -6,21 +6,31 @@ module and passes a list of arguments to it. The arguments are the same as those
 passed to the command line interface (CLI) of `e3sm_to_cmip`.
 The script can be run from the command line or an IDE.
 """
+
+import datetime
+
 from e3sm_to_cmip.__main__ import main
+
+# The list of variables to process. Update as needed.
+VAR_LIST = "pfull, phalf, tas, ts, psl, ps, sfcWind, huss, pr, prc, prsn, evspsbl, tauu, tauv, hfls, clt, rlds, rlus, rsds, rsus, hfss, cl, clw, cli, clivi, clwvi, prw, rldscs, rlut, rlutcs, rsdt, rsuscs, rsut, rsutcs, rtmt, abs550aer, od550aer, rsdscs, hur"
+
+# The output path for CMORized datasets. Update as needed.
+OUTPUT_PATH = f"../qa/run_{datetime.datetime.now().strftime('%Y%m%d_%H%M%S')}"
 
 args = [
     "--var-list",
-    'pfull, phalf, tas, ts, psl, ps, sfcWind, huss, pr, prc, prsn, evspsbl, tauu, tauv, hfls, clt, rlds, rlus, rsds, rsus, hfss, cl, clw, cli, clivi, clwvi, prw, rldscs, rlut, rlutcs, rsdt, rsuscs, rsut, rsutcs, rtmt, abs550aer, od550aer, rsdscs, hur',
+    f"{VAR_LIST}",
     "--input",
-    "/lcrc/group/e3sm/e3sm_to_cmip/input/atm-unified-eam-ncclimo",
+    "/lcrc/group/e3sm/e3sm_to_cmip/test-cases/atm-unified-eam/input-regridded",
     "--output",
-    "../qa/tmp",
+    f"{OUTPUT_PATH}",
     "--tables-path",
     "/lcrc/group/e3sm/e3sm_to_cmip/cmip6-cmor-tables/Tables/",
     "--user-metadata",
-    "/lcrc/group/e3sm/e3sm_to_cmip/template.json",
-    "--serial"
+    "/lcrc/group/e3sm/e3sm_to_cmip/CMIP6-Metadata/template.json",
+    "--serial",
 ]
 
-# `main()` creates an `E3SMtoCMIP` object and passes `args` to it, which sets the object parameters to execute a run.
+# `main()` creates an `E3SMtoCMIP` object and passes `args` to it, which sets
+# the object parameters to execute a run.
 main(args)
